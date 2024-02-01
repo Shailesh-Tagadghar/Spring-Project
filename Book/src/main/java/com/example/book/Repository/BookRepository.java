e7dfa1a67bd52a0e1525bf92e7c35eecf5fe65e2
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-02-01 14:34:12 +0530

    BookRepository.java

--- Book/src/main/java/com/example/book/Repository/BookRepository.java
+++ Book/src/main/java/com/example/book/Repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.example.book.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.book.Model.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
